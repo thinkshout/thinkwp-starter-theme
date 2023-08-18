4c52027fc547b66e74fcc3f4313f7d36577b6355
James Macon <4700339+jamacon36@users.noreply.github.com>
2023-08-18 12:00:54 -0400

    adds accordion block to theme as example

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
 // IMPORTS GO HERE
 import accessibleMenu from "./components/accessible-menu";
+import accordions from "./components/accordion";
+
 (($) => {
   $(document).ready(() => {
     // BEGIN CUSTOM CODE GOES HERE
     accessibleMenu($);
+    accordions();
     // END CUSTOM CODE GOES HERE
   });
 })(jQuery);
