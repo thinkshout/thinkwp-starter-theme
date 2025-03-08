f2504f1457424b69d17c615b9cc68a61d9095f00
Jaymz Rhime <wxactly@gmail.com>
2025-03-08 14:37:17 -0700

    Fixing includes for front page template

--- page.php
+++ page.php
@@ -21,4 +21,10 @@ if ( 'style-guide' === $post->post_name ) {
 	$context['ts_blocks'] = $acf_block_types->get_data();
 }
 
-Timber::render( array( 'pages/page-' . $post->post_name . '.twig', 'pages/page.twig' ), $context );
+$templates = array( 'pages/page-' . $post->post_name . '.twig', 'pages/page.twig' );
+
+if ( is_front_page() ) {
+	array_unshift( $templates, 'pages/front-page.twig' );
+}
+
+Timber::render( $templates, $context );
