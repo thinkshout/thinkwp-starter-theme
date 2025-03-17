2478f01555ed96bb9478ad8f53b1a4df98c620a1
Jaymz Rhime <wxactly@gmail.com>
2025-03-17 14:22:34 -0600

    Remove references to scripts that no longer exist

--- src/StarterSite.php
+++ src/StarterSite.php
@@ -245,10 +245,6 @@ class StarterSite extends Site {
 			)
 		);
 
-		wp_enqueue_script( 'thinktimber-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), $this->scripts_version, true );
-
-		wp_enqueue_script( 'thinktimber-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), $this->scripts_version, true );
-
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
