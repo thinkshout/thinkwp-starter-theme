948f857984c606aa97c9ba748f1546de1f74a780
Jaymz Rhime <wxactly@gmail.com>
2025-10-14 08:47:47 -0600

    Pick up component documentation from twig to show on the style guide

--- page.php
+++ page.php
@@ -15,27 +15,6 @@ use Timber\Timber;
 $context = Timber::context();
 $post = $context['post'];
 
-// Check if this is the style guide page.
-if ( 'style-guide' === $post->post_name ) {
-  $styleguide_directory_scans = array(
-    'ts_blocks' => '/views/organisms/blocks',
-    'ts_molecules' => '/views/molecules',
-  );
-  foreach( $styleguide_directory_scans as $context_label => $theme_path ) {
-    $context[$context_label] = [];
-    $directory = get_template_directory() . $theme_path;
-    $element_directories = scandir($directory);
-    if ($element_directories) {
-      foreach ($element_directories as $id) {
-        $styleguide_file = "$directory/$id/styleguide/$id--styleguide-layout.twig";
-        if ( file_exists( $styleguide_file ) ) {
-          $context[$context_label][$id] = ucfirst(str_replace('-', ' ', $id));
-        }
-      }
-    }
-  }
-}
-
 $templates = array( 'pages/page-' . $post->post_name . '.twig', 'pages/page.twig' );
 
 if ( is_front_page() ) {
